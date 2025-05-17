7e085d75e6da557a111fe4ade05fd70970316736
Tejas Shah <tejasshah2k19@gmail.com>
2025-05-17 17:09:10 +0530

    list subject

--- src/app/list-subject/list-subject.component.ts
+++ src/app/list-subject/list-subject.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { SubjectService } from '../service/subject.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list-subject',
-  imports: [],
+  imports: [HttpClientModule,NgFor],
+  providers: [SubjectService],
   templateUrl: './list-subject.component.html',
   styleUrl: './list-subject.component.css'
 })
 export class ListSubjectComponent {
+
+  data:Array<any> = []
+
+  constructor(private subjectService: SubjectService) {
+    this.getAllSubjects();
+  }
+
+  getAllSubjects() {
+    this.subjectService.getAllSubjects().subscribe(resp => {
+      console.log(resp);
+      this.data = resp  
+    }, err => {
+      console.log(err);
+    })
+  }
 
 }
